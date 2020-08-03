5c33bf804f2f5ebe76f0e4cbf837cb4198d79a28
eg@protonmail.com <1qaz2wsx>
2020-08-03 15:58:13 +0800

    Show how to use after, before, fail, functions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Queue::after(function (JobProcessed $event) {
+            // $event->connectionName
+            // $event->job
+            // $event->job->payload()
+            echo "JobProcessed(after), '$event->connectionName'" . PHP_EOL;
+        });
+        Queue::before(function (JobProcessing $event) {
+            // $event->connectionName
+            // $event->job
+            // $event->job->payload()
+            echo "JobProcessing(before), '$event->connectionName'" . PHP_EOL;
+        });
+        Queue::failing(function (JobFailed $event) {
+            // $event->connectionName
+            // $event->job
+            // $event->exception
+            echo "JobFailed(failing), '$event->connectionName'" . PHP_EOL;
+        });
+//        Queue::looping();
     }
 }
